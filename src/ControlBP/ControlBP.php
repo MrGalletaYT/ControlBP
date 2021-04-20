e8cbfa0e682d6091b3883317b78f03ab04a72806
ByPaps <79675866+ByPaps@users.noreply.github.com>
2021-04-20 11:28:07 -0500

    Update ControlBP.php

--- src/ControlBP/ControlBP.php
+++ src/ControlBP/ControlBP.php
@@ -7,15 +7,12 @@ use pocketmine\command\CommandSender;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityMoveEvent;
-use icontrolu\InventoryUpdateTask;
-use pocketmine\event\inventory\InventoryPickupItemEvent;
-use pocketmine\event\Listener;
+use controlbp\InventoryUpdateTask;
 use pocketmine\event\player\PlayerAnimationEvent;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\event\player\PlayerDropItemEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\network\mcpe\protocol\AnimatePacket;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\network\mcpe\protocol\AnimatePacket;
